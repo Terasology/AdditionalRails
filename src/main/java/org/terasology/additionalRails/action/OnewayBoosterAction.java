c2aeebf797cf249cab25fe6a39565c0c749ba7b8
busyVision <>
2017-12-27 18:14:36 +0900

    Implement one-way boosting, line_connection working

--- src/main/java/org/terasology/additionalRails/action/OnewayBoosterAction.java
+++ src/main/java/org/terasology/additionalRails/action/OnewayBoosterAction.java
@@ -16,6 +16,8 @@
 package org.terasology.additionalRails.action;
 
 import com.google.common.collect.Sets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.additionalRails.components.OnewayBoosterRailComponent;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
@@ -23,12 +25,18 @@ import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.math.Rotation;
 import org.terasology.math.geom.Vector3f;
 import org.terasology.minecarts.blocks.RailComponent;
+import org.terasology.minecarts.blocks.RailsUpdateFamily;
 import org.terasology.minecarts.components.RailVehicleComponent;
+import org.terasology.registry.In;
 import org.terasology.segmentedpaths.components.PathFollowerComponent;
 import org.terasology.segmentedpaths.events.OnExitSegment;
 import org.terasology.segmentedpaths.events.OnVisitSegment;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockComponent;
+import org.terasology.world.block.BlockManager;
 
 import java.util.Objects;
 import java.util.Set;
@@ -36,7 +44,11 @@ import java.util.Set;
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class OnewayBoosterAction extends BaseComponentSystem implements UpdateSubscriberSystem {
     private Set<RailCart> entities = Sets.newHashSet();
-    private static final int ACCELERATION_RATE = 10;
+    private static final int PUSH_RATE = 20, VELOCITY_LENGTH_MAX = 25;
+    @In
+    private BlockManager blockManager;
+
+    private static final Logger logger = LoggerFactory.getLogger(OnewayBoosterAction.class);
 
     @ReceiveEvent(components = {OnewayBoosterRailComponent.class, RailComponent.class})
     public void onEnterBoosterSegment(OnVisitSegment event, EntityRef entity) { //entity is the rail and event.getSegmentEntity() the cart
@@ -53,23 +65,26 @@ public class OnewayBoosterAction extends BaseComponentSystem implements UpdateSu
         entities.removeIf(e -> !e.cart.exists() || !e.cart.hasComponent(RailVehicleComponent.class) || !e.cart.hasComponent(PathFollowerComponent.class)
                 || !e.rail.exists() || !e.rail.hasComponent(OnewayBoosterRailComponent.class));
         for(RailCart rc : entities) {
-            accelerate(rc.cart, delta);
+            Block block = rc.rail.getComponent(BlockComponent.class).getBlock();
+            RailsUpdateFamily family = (RailsUpdateFamily) block.getBlockFamily();
+            Rotation rotation = family.getRotationFor(block.getURI());
+            float yaw = rotation.getYaw().getRadians();
+            Vector3f railDirection = new Vector3f((float) Math.cos(yaw), 0, (float) Math.sin(yaw)); //https://stackoverflow.com/a/1568687
+            if(family == blockManager.getBlockFamily("AdditionalRails:OnewayBoosterRailInverted"))
+                railDirection.invert();
+            //Currently the direction the tiles point to is the opposite of the real direction they send carts to on Z axis.
+            //TODO: fix this fundamentally
+            railDirection.mulZ(-1);
+            push(rc.cart, railDirection.mul(PUSH_RATE * delta));
         }
     }
-    /**Adds {@link OnewayBoosterAction#ACCELERATION_RATE} per delta to x or z of the velocity of {@code ref}'s {@code RailVehicleComponent}*/
-    private void accelerate(EntityRef ref, float delta) {
+    /**Adds {@code thisMuch} to the velocity of {@code ref}'s {@code RailVehicleComponent}
+     * if it doesn't exceed {@link #VELOCITY_LENGTH_MAX}.*/
+    private void push(EntityRef ref, Vector3f thisMuch) {
         RailVehicleComponent railVehicleComponent = ref.getComponent(RailVehicleComponent.class);
-        Vector3f velocity = railVehicleComponent.velocity, accel;
-        boolean isGoingOpposite;
-        if(Math.abs(velocity.x) > Math.abs(velocity.z)) {
-            accel = new Vector3f(ACCELERATION_RATE * delta, 0, 0);
-            isGoingOpposite = (accel.x < 0) != (velocity.x < 0);
-        } else {
-            accel = new Vector3f(0, 0, ACCELERATION_RATE * delta);
-            isGoingOpposite = (accel.z < 0) != (velocity.z < 0);
-        }
-        if(velocity.lengthSquared() < 25f || isGoingOpposite) {
-            velocity.add(accel);
+        Vector3f velocity = railVehicleComponent.velocity;
+        if(velocity.lengthSquared() < VELOCITY_LENGTH_MAX) {
+            velocity.add(thisMuch);
             ref.saveComponent(railVehicleComponent);
         }
     }
@@ -92,18 +107,4 @@ public class OnewayBoosterAction extends BaseComponentSystem implements UpdateSu
             return Objects.hash(rail, cart);
         }
     }
-    /*private void printDebug(EntityRef rail, EntityRef cart) {
-    LocationComponent cartL = cart.getComponent(LocationComponent.class);
-    RailVehicleComponent cartV = cart.getComponent(RailVehicleComponent.class);
-    PathFollowerComponent cartP = cart.getComponent(PathFollowerComponent.class);
-    LocationComponent railL = rail.getComponent(LocationComponent.class);
-    logger.warn("cart : ");
-    logger.warn("world position : " + cartL.getWorldPosition() + ", local position : " + cartL.getLocalPosition());
-    logger.warn("local rotation : " + cartL.getLocalRotation() + ", world rotation : " + cartL.getWorldRotation());
-    logger.warn("heading : " + cartP.heading);
-    logger.warn("velocity : " + cartV.velocity);
-    logger.warn("rail : ");
-    logger.warn("world position : " + railL.getWorldPosition() + ", local position : " + railL.getLocalPosition());
-    logger.warn("local rotation : " + railL.getLocalRotation() + ", world rotation : " + railL.getWorldRotation());
-    }*/
 }
