e564f99bd362a52f6744b0e1314f09ad6ad8aa33
busyVision <>
2017-12-27 23:37:56 +0900

    Remove unnecessary imports

--- src/main/java/org/terasology/additionalRails/action/OnewayBoosterAction.java
+++ src/main/java/org/terasology/additionalRails/action/OnewayBoosterAction.java
@@ -16,8 +16,6 @@
 package org.terasology.additionalRails.action;
 
 import com.google.common.collect.Sets;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.terasology.additionalRails.components.OnewayBoosterRailComponent;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.entitySystem.entity.EntityRef;
