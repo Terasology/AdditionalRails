9a3208f3d5e8f6c0a631793610dede05d00f5a8b
anuar2k <anuar2k@outlook.com>
2018-01-03 21:32:49 +0100

    Hoover Cart

---
+++ src/main/java/org/terasology/additionalRails/components/HooverCartComponent.java
@@ -0,0 +1,10 @@
+package org.terasology.additionalRails.components;
+
+import org.terasology.entitySystem.Component;
+
+/**
+ * Component used to differentiate Hoover Cart from other ones.
+ */
+public class HooverCartComponent implements Component {
+    public int energy = 0;
+}
---
+++ src/main/java/org/terasology/additionalRails/ui/HooverCartScreen.java
@@ -0,0 +1,47 @@
+package org.terasology.additionalRails.ui;
+
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.logic.characters.CharacterComponent;
+import org.terasology.logic.players.LocalPlayer;
+import org.terasology.registry.In;
+import org.terasology.rendering.nui.CoreScreenLayer;
+import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
+import org.terasology.rendering.nui.layers.ingame.inventory.InventoryGrid;
+
+/**
+ * Screen opened up upon activation of the Hoover Cart - used to put rails into it.
+ */
+public class HooverCartScreen extends CoreScreenLayer {
+
+    @In
+    private LocalPlayer localPlayer;
+
+    private InventoryGrid containerInventory;
+
+    @Override
+    public void initialise() {
+        InventoryGrid inventory = find("inventory", InventoryGrid.class);
+        inventory.bindTargetEntity(new ReadOnlyBinding<EntityRef>() {
+            @Override
+            public EntityRef get() {
+                return localPlayer.getCharacterEntity();
+            }
+        });
+        inventory.setCellOffset(10);
+
+        containerInventory = find("container", InventoryGrid.class);
+        containerInventory.bindTargetEntity(new ReadOnlyBinding<EntityRef>() {
+            @Override
+            public EntityRef get() {
+                EntityRef characterEntity = localPlayer.getCharacterEntity();
+                CharacterComponent characterComponent = characterEntity.getComponent(CharacterComponent.class);
+                return characterComponent.predictedInteractionTarget;
+            }
+        });
+    }
+
+    @Override
+    public boolean isModal() {
+        return false;
+    }
+}
