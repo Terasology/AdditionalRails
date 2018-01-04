dc6930e437485208d2a24bc7bfd29ebfb4d5dc3b
Alberto <alberto512c@gmail.com>
2018-01-04 10:48:11 +0100

    Create harvesting cart

--- src/main/java/org/terasology/additionalRails/action/HarvestAction.java
+++ src/main/java/org/terasology/additionalRails/action/HarvestAction.java
@@ -15,9 +15,6 @@
  */
 package org.terasology.additionalRails.action;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.terasology.additionalRails.components.HarvestCartComponent;
 import org.terasology.additionalRails.events.CartActivatedEvent;
 import org.terasology.entitySystem.entity.EntityRef;
@@ -36,44 +33,42 @@ import org.terasology.world.block.BlockComponent;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class HarvestAction extends BaseComponentSystem{
-
     public int currentStage;
-    private static final Logger logger = LoggerFactory.getLogger(HarvestAction.class);
+
     @In
     private BlockEntityRegistry blockEntityRegistry;
+
     @ReceiveEvent(components = {HarvestCartComponent.class})
     public void cartActivatedEvent(CartActivatedEvent event, EntityRef entity) {
-        logger.info("Activated Harvest Cart ID: {}", entity.getId());
 
-        PathFollowerComponent pfComp = entity.getComponent(PathFollowerComponent.class);
-        EntityRef rbEntity = pfComp.segmentMeta.association;
+        PathFollowerComponent pfComponent = entity.getComponent(PathFollowerComponent.class);
+        EntityRef entityref = pfComponent.segmentMeta.association;
 
-        BlockComponent bComp = rbEntity.getComponent(BlockComponent.class);
-        Vector3i rbLocation = new Vector3i(bComp.getPosition());
+        BlockComponent blockcomponent = entityref.getComponent(BlockComponent.class);
+        Vector3i location = new Vector3i(blockcomponent.getPosition());
 
-        Direction direction = Direction.inDirection(pfComp.heading);
+        Direction direction = Direction.inDirection(pfComponent.heading);
         Vector3i leftVector = direction.toSide().yawClockwise(1).getVector3i();
         Vector3i rightVector = direction.toSide().yawClockwise(3).getVector3i();
 
-        Vector3i firstBlockPosition = new Vector3i(rbLocation).add(leftVector);
-        Vector3i secondBlockPosition = new Vector3i(rbLocation).add(rightVector);
+        Vector3i leftPosition = new Vector3i(location).add(leftVector);
+        Vector3i rightPosition = new Vector3i(location).add(rightVector);
 
+        EntityRef block1=blockEntityRegistry.getExistingEntityAt(leftPosition);
+        EntityRef block2=blockEntityRegistry.getExistingEntityAt(rightPosition);
 
-
-        EntityRef block1=blockEntityRegistry.getExistingEntityAt(firstBlockPosition);
-        EntityRef block2=blockEntityRegistry.getExistingEntityAt(secondBlockPosition);
         BushDefinitionComponent component1 = block1.getComponent(BushDefinitionComponent.class);
         BushDefinitionComponent component2 = block2.getComponent(BushDefinitionComponent.class);
+
         if(component1!=null){
-            if(component1.currentStage==1){
+            if(component1.currentStage==3){
                 entity.send(new ActivateEvent(block1, entity, null, null, null, null, 0));
             }
         }
         if(component2!=null){
-            if(component1.currentStage==1){
+            if(component2.currentStage==3){
                 entity.send(new ActivateEvent(block2, entity, null, null, null, null, 0));
             }
         }
     }
 }
-
