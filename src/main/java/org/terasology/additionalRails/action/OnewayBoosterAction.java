69159eba07b79697d5ac9a7269a96a7d6ab8a79a
Tobias Nett <skaldarnar@googlemail.com>
2021-02-06 18:34:59 +0100

    feat(JOML): migrate `BlockComponent` (#46)

--- src/main/java/org/terasology/additionalRails/action/OnewayBoosterAction.java
+++ src/main/java/org/terasology/additionalRails/action/OnewayBoosterAction.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.additionalRails.action;
 
 import com.google.common.collect.Sets;
@@ -70,7 +57,7 @@ public class OnewayBoosterAction extends BaseComponentSystem implements UpdateSu
         entities.removeIf(e -> !e.cart.exists() || !e.cart.hasComponent(RailVehicleComponent.class) || !e.cart.hasComponent(PathFollowerComponent.class)
                 || !e.rail.exists() || !e.rail.hasComponent(OnewayBoosterRailComponent.class));
         for (RailCart rc : entities) {
-            Block block = rc.rail.getComponent(BlockComponent.class).getBlock();
+            Block block = rc.rail.getComponent(BlockComponent.class).block;
             RailBlockFamily family = (RailBlockFamily) block.getBlockFamily();
             //Get the direction of the rail
             Rotation rotation = family.getRotationFor(block.getURI());
